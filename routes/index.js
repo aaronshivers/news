7876a618a824a71f0811b9cec50c0e3a0138b3b7
aaronshivers <aaron@moonmail.org>
2019-04-09 18:20:50 -0500

    added dotenv

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const fetch = require('node-fetch')
 
-router.get('/', (req, res) => res.render(`index`))
+const { API_KEY } = process.env
+const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${ API_KEY }`
 
+
+// GET /
+router.get('/', (req, res) => {
+
+  try {
+
+    // render home
+    res.send(url)
+
+  } catch (error) {
+
+    // send error message
+    console.log(error.message)
+    // res.render('error', { msg: error.message })
+  }
+})
 module.exports = router
