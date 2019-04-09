7f4b4ca2fd099f9937c3a6269f35f0c452a9c259
aaronshivers <aaron@moonmail.org>
2019-04-09 18:13:57 -0500

    moved GET / to routes/index.js

--- news.js
+++ news.js
@@ -4,11 +4,13 @@ const fetch = require('node-fetch')
 
 const port = process.env.PORT || 3000
 
+const indexRoutes = require('./routes/index')
+
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.render(`index`))
+app.use(indexRoutes)
 
 app.listen(port, () => console.log(`Server running on port ${ port }.`))
 
