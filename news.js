84aeb0960354208ddfd1d591b109c2ad328cc11f
aaronshivers <aaron@moonmail.org>
2019-04-09 18:51:52 -0500

    added helmet and compression

--- news.js
+++ news.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const compression = require('compression')
+const helmet = require('helmet')
 
 const port = process.env.PORT || 3000
 
@@ -9,6 +11,8 @@ const indexRoutes = require('./routes/index')
 
 app.set('view engine', 'ejs')
 
+app.use(helmet())
+app.use(compression())
 app.use(express.static('public'))
 
 app.use(indexRoutes)
