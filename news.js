3687046d5963eacbf91eb8904307ebdb25e702d2
aaronshivers <aaron@moonmail.org>
2019-04-09 17:47:07 -0500

    added bootstrap css

--- news.js
+++ news.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => res.render(`index`))
 
 app.listen(port, () => console.log(`Server running on port ${ port }.`))
